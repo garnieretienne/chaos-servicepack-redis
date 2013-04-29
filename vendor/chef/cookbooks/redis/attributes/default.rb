2072c18375e7a73b8f6a7a1b095d0894b14c583b
Etienne Garnier <garnier.etienne@gmail.com>
2013-04-29 21:27:51 +0200

    return a redis url and customize configuration using REDIS_ADDRESS, REDIS_PORT and REDIS_PASSWORD

---
+++ vendor/chef/cookbooks/redis/attributes/default.rb
@@ -0,0 +1,3 @@
+default['redis']['address']  = '127.0.0.1'
+default['redis']['port']     = 6379
+default['redis']['password'] = nil
--- vendor/chef/cookbooks/redis/recipes/default.rb
+++ vendor/chef/cookbooks/redis/recipes/default.rb
@@ -12,3 +12,19 @@ package "redis server" do
   package_name "redis-server"
   action :upgrade
 end
+
+# Ensure the service is running
+service "redis" do
+  service_name "redis-server"
+  action :enable
+end
+
+# Generate redis configuration file
+template "redis conf" do
+  path "/etc/redis/redis.conf"
+  source "redis.conf.erb"
+  owner "root"
+  group "root"
+  mode 0600
+  notifies :reload, 'service[redis]', :immediately
+end
