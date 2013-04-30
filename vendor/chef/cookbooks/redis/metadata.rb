0ea240c13c990c74f2a72de6ecc35504335680e7
Etienne Garnier <garnier.etienne@gmail.com>
2013-04-30 23:53:30 +0200

    add cookbook description for redis

--- vendor/chef/cookbooks/redis/metadata.rb
+++ vendor/chef/cookbooks/redis/metadata.rb
@@ -3,5 +3,5 @@ maintainer       'Chaos'
 maintainer_email 'garnier.etienne@gmail.com'
 license          'MIT License'
 description      'Install and configure Redis stack using the package manager'
-long_description IO.read(File.join(File.dirname(__FILE__)))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
